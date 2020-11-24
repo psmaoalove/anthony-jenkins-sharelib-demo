5c883f4957f0fb5b8c7268487175c14efde8dfe9
奔跑在麦田里 <despairdog@126.com>
2020-11-24 11:13:50 +0800

    Update TwoNumberAdd.groovy

--- vars/TwoNumberAdd.groovy
+++ vars/TwoNumberAdd.groovy
@@ -3,17 +3,11 @@ def call(int a, int b) {
     
 pipeline {
     agent  any
-    libraries{
-        lib('devops-demo')
-    }
     stages {
         //下载代码
         stage("GetCode"){ //阶段名称
             steps{  //步骤
-script{
-    result =TwoNumberAdd 3,5
-    println result
-}
+             echo '333'
             }
         }
     }

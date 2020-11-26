17f9f937583e71aba9fc05fc8554a728e085a9d5
奔跑在麦田里 <despairdog@126.com>
2020-11-26 10:04:49 +0800

    Update TwoNumberAdd.groovy

--- vars/TwoNumberAdd.groovy
+++ vars/TwoNumberAdd.groovy
@@ -1,17 +1,18 @@
 
 def call(int a, int b) {
-    
-pipeline {
-    agent  any
-    stages {
-        //下载代码
-        stage("GetCode"){ //阶段名称
-            steps{  //步骤
-             echo '333'
+        try {
+            timeout(1){
+                //等待sonarqube的分析结果，这里是等1分钟
+                echo "未通过SonarQube的代码检查，请及时修改!"
+                currentBuild.result = 'FAILED'
             }
+        }catch (exc){
+            echo 'Something failed, I should sound the klaxons!'
         }
     }
 
-}
 
-}
+
+
+
+
